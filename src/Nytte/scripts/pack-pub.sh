e6bafa05d11b507af56306b74ec14801ec55919d
William Mumby <williammumby@Williams-MacBook-Pro.local>
2021-01-13 13:30:32 +0000

    updated script

--- src/Nytte/scripts/pack-pub.sh
+++ src/Nytte/scripts/pack-pub.sh
@@ -4,6 +4,7 @@ if [ $TRAVIS_BRANCH = "staging" ]
 then
     echo Updating Nytte alpha package $TRAVIS_BRANCH
     dotnet pack -c Release /p:PackageVersion:0.1.$TRAVIS_BUILD_NUMBER-alpha -o .
+    dotnet nuget push *.nupkg -k $NUGET_API_KEY -s https://api.nuget.org/v3/index.json
 fi
 
-dotnet nuget push *.nupkg -k $NUGET_API_KEY -s https://api.nuget.org/v3/index.json
+
