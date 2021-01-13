9f86d13fe2378aa525d1a8cd00a19ec553ea2d72
William Mumby <williammumby@Williams-MacBook-Pro.local>
2021-01-13 15:00:56 +0000

    only pack on nonpr

--- scripts/pack.sh
+++ scripts/pack.sh
@@ -1,13 +1,14 @@
-echo "Starting package publish"
+if [$TRAVIS_PULL_REQUEST != "false"]
+then
+    echo "Starting package publish"
 
-for dir in src/*/
-do
-    dir=${dir%*/}
-    echo Publishing nuget package $dir
-    sh ./$dir/scripts/pack-pub.sh & wait
-    echo ""
-done
-
-
-
-echo "Finished publishing package"
+    for dir in src/*/
+    do
+        dir=${dir%*/}
+        echo Publishing nuget package $dir
+        sh ./$dir/scripts/pack-pub.sh & wait
+        echo ""
+    done
+    
+    echo "Finished publishing package"
+fi
