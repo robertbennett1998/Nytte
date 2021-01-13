f53da9adc2e1d7051415e84460340c554356bb1a
William Mumby <williammumby@Williams-MacBook-Pro.local>
2021-01-13 14:14:06 +0000

    added http package to build

--- src/Nytte.Http/scripts/pack-pub.sh
+++ src/Nytte.Http/scripts/pack-pub.sh
@@ -1,1 +1,8 @@
-echo pack
+cd src/Nytte.Http
+
+if [ $TRAVIS_BRANCH = "staging" ]
+then
+    echo Updating Nytte alpha package $TRAVIS_BRANCH
+    dotnet pack -c Release /p:PackageVersion=0.4.$TRAVIS_BUILD_NUMBER-alpha -o .
+    dotnet nuget push *.nupkg -k $NUGET_API_KEY -s https://api.nuget.org/v3/index.json
+fi
