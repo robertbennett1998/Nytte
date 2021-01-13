6ca14ba7be14fec273af1dc6400abb04177468e6
William Mumby <williammumby@Williams-MacBook-Pro.local>
2021-01-13 14:52:46 +0000

    updated readme and started sample code

--- README.md
+++ README.md
@@ -1,2 +1,11 @@
 # Nytte
 A library of utilities helpful for projects.
+
+> Please checkout the *staging* branch for the *alpha* releases.
+
+## Getting Started
+To get started with any of the packages find them on Nuget.
+
+A simple way to see these in action is to checkout the [samples]()
+
+Taking the Nytte.Http package as an example use ```dotnet add package Nytte.Http --version 0.4.7-alpha``` to add the package to your project.
---
+++ samples/Nytte.Web.Sample/Startup.cs
@@ -0,0 +1,61 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Http;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Hosting;
+using Nytte.Http;
+
+namespace Nytte.Web.Sample
+{
+    public record AJoke(int Id, string Type, string Setup, string Punchline);
+    public class Startup
+    {
+        // This method gets called by the runtime. Use this method to add services to the container.
+        // For more information on how to configure your application, visit https://go.microsoft.com/fwlink/?LinkID=398940
+        public void ConfigureServices(IServiceCollection services)
+        {
+            services.AddNytte()
+                .AddHttp();
+        }
+
+        // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
+        public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
+        {
+            if (env.IsDevelopment())
+            {
+                app.UseDeveloperExceptionPage();
+            }
+
+            var httpClient = app.ApplicationServices.GetRequiredService<IHttpClient>();
+
+            app.UseRouting();
+
+            app.UseEndpoints(endpoints =>
+            {
+                endpoints.MapGet("/", async context =>
+                {
+                    var url = "https://official-joke-api.appspot.com/random_joke";
+                    var result = await httpClient.GetAsync<AJoke>(url);
+                    if (result.RefusedConnection)
+                    {
+                        await context.Response.WriteAsync(
+                            $"uh oh the joke service appears to be offline using url: {url}");
+                    }
+                    else if (result.Data is { })
+                    {
+                        var joke = result.Data;
+                        await context.Response.WriteAsync($"{joke.Setup} ... \n {joke.Punchline}");
+                    }
+                    else
+                    {
+                        await context.Response.WriteAsync($"Uh oh request failed with status code: {result.Message.StatusCode}");   
+                    }
+                });
+            });
+        }
+    }
+}
